086d27beab89b2069bbe22a54d6dc6d42b15e4b8
ykiyoshima <y.kiyoshima.271@gmail.com>
2023-03-01 02:35:32 +0000

    update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,3 +2,6 @@
 
 cp /home/site/wwwroot/scripts/default /etc/nginx/sites-available/default
 service nginx reload
+
+cp /home/site/wwwroot/.env.example /home/site/wwwroot/.env
+php artisan key:generate
